ec3d335632af11bfac9f9204d5540caa6d9e297b
Gabriel <gkime91@gmail.com>
2022-06-21 22:34:28 -0700

    wrote first server test, whoooo.

--- __tests__/servertest.js
+++ __tests__/servertest.js
@@ -2,3 +2,14 @@ const supertest = require('supertest');
 const fs = require('fs');
 
 const server = 'http://localhost:3000';
+
+describe('backend main page', () => {
+  describe('GET', () => {
+    it ('responds with 200 OK status and text/html content type', () => {
+      return supertest(server)
+        .get('/')
+        .expect('Content-Type', /text\/html/)
+        .expect(200);
+    })
+  });
+})
