cb906655eedf8d09dfc94318b1ffd486ad8f62c5
Gabriel <gkime91@gmail.com>
2023-01-18 15:53:56 -0800

    basic functionality for plantInfo modal working.

--- client/components/PlantInfo.jsx
+++ client/components/PlantInfo.jsx
@@ -7,10 +7,28 @@ class PlantInfo extends Component {
     super(props); 
 
     this.state = {
-      editPlant: false
+      editPlant: false,
+      plantLabelMap: {
+        name: 'Name ',
+        img: null,
+        light: 'Light: ',
+        soil: 'Soil: ',
+        fertilizer: 'Fertilizer: ',
+        notes: 'Notes: ',
+        watering_schedule: 'Water Every: ',
+        fertilizing_schedule: 'Fertilize Every: ',
+        days: ' Days',
+        weeks: ' Weeks',
+        months: ' Months',
+        watering_time_of_day: 'Water in the: ',
+        fertilizing_time_of_day: 'Fertilize in the: ',
+        morning: ' Morning',
+        midday: ' Afternoon',
+        evening: ' Evening'
+      },
     }
 
-    this.populateInfo = this.populateInfo.bind(this);
+    this.mapLabelsToProperties = this.mapLabelsToProperties.bind(this);
     this.toggleEditPlant = this.toggleEditPlant.bind(this);
   }
 
@@ -19,136 +37,108 @@ class PlantInfo extends Component {
       editPlant: this.state.editPlant === false ? this.state.editPlant = true : this.state.editPlant = false
     })
   };
+
   
-  populateInfo () {
-    const { plantInfo } = this.props;
-    const infoLabels = [
-      ['name', ' Name'], 
-      ['light', ' Light Preference'], 
-      ['soil', ' Soil Preference'], 
-      ['fertilizer', ' Fertilizer Preference'], 
-      ['notes', ' Notes'], 
-      ['date', ' Schedule'], 
-      ['tod', ' Watering Time'], 
-      ['mist', ' Mist']];
-
-    const flattenObj = (obj) => {
-      const flattenedObj = Object.entries(obj).filter((entry) => (entry[1]))
-      return flattenedObj;
-    };
-
-    const renderFlattenedObjs= (flattenedObj) => {
-      const labels = {
-        'morning': 'Morning',
-        'mid': 'Afternoon', 
-        'evening': 'Evening'
-      }
-      const fragments = flattenedObj.map((entry, index) => {
-        // for schedule info
-        if (entry[0] === 'morning' || entry[0] === 'mid' || entry[0] === 'evening') return (
-          <React.Fragment key={`entry${index}`}>
-            {labels[entry[0]]}&nbsp; 
-          </React.Fragment>
-        )
-        // for time of day
-        else return (
-          <React.Fragment key={`entry${index}`}>
-            {entry[0]}: {entry[1]}&nbsp; 
-          </React.Fragment>
-        );
-       
-      });
-
-      return (
-        <span>{fragments}</span>
+  mapLabelsToProperties () {
+    const { plantProperties } = this.props;
+    const labelMap = this.state.plantLabelMap;
+    // initial pass:
+    // iterate through plant properties and filter for string values.
+    const stringValues = Object.entries(plantProperties).filter(entry => typeof entry[1] === 'string');
+    const labeledStringAttributes = stringValues.map((entry, index) => {
+      const key = `att${index}`
+      const [ property, value ] = entry;
+      const label = labelMap[property];
+      return(
+        <label key={key}>
+          {label}
+          <span>
+            {value}
+          </span>
+        </label>
       );
-    };
-
-    const chosenDate = flattenObj(plantInfo.date);
-    const chosenTod = flattenObj(plantInfo.tod);
-
-    return infoLabels.map((label, index) => {
-      const key = `field${index}`;
-      if (label[0] === 'date') return (
-        <PlantDetail
-          key={key}
-          infoLabel={label[1]}
-          info={renderFlattenedObjs(chosenDate)}
-        />
-      )
-      else if (label[0] === 'tod') return (
-        <PlantDetail
-          key={key}
-          infoLabel={label[1]}
-          info={renderFlattenedObjs(chosenTod)}
-        />
-      )
-      else if (label[0] === 'mist') return (
-        <label key={key} >
-          {label[0]}
-          <input type="checkbox" checked={plantInfo.mist} disabled />
+    });
+    // second pass:
+    // iterate through plant properties and filter for object values.
+    const scheduleObjValues = Object.entries(plantProperties).filter((entry) => typeof entry[1] === 'object' && entry[0] !== 'img');
+    
+    const labeledSchedules = scheduleObjValues.map((entry, index) => {
+      const key =`schedule${index}`;
+      const [ property, scheduleObj] = entry;
+      const label = labelMap[property];
+      // label the schedule values
+      const labeledScheduleValues = Object.entries(scheduleObj).map((entry, index) => {
+        const [ property, value ] = entry;
+        const key= `scheduleVal${index}`;
+        return (
+          <span key={key}>
+            {value}
+            <label >
+              {labelMap[property]}&nbsp;
+            </label>
+          </span>
+          
+        );
+      }); 
+      // label the schedule
+      return(
+        <label key={key}>
+          {label}
+          <span>
+            {labeledScheduleValues}
+          </span>
         </label>
       )
-      else return (
-          <PlantDetail
-            key={key} 
-            infoLabel={label[1]} 
-            info={plantInfo[label[0]]}
-          />
-      );  
-    });  
+    });
+
+    // return labeled string attributes and labeled schedules
+    return [labeledStringAttributes, labeledSchedules];
   };
 
   render() {
+    const labeledAttributes = this.mapLabelsToProperties()[0];
+    const labeledSchedules = this.mapLabelsToProperties()[1];
     const {
       index,
       editedPlant,
       clonePlant,
       editPlant,
       savePlantEdits,
-      modalVisible,
       setPlantState,
       toggleModal
     } = this.props;
 
-    const plantInfo = this.populateInfo();
     // if no modal opened (either info modal or edit modal), don't render anything. 
-    if (!modalVisible) return null;
     // if modal is open but plant isn't being edited, return plant info. 
     if (!this.state.editPlant) {
       return (
-      <main className="plant-info-overlay">
         <div className="plant-modal">
           <div id="info-modal-buttons">
             <button onClick={() => {
-              // copy the state of the plant to be edited to isolated edited plant state object 
-              clonePlant(index);
               this.toggleEditPlant();
               }
             }>Edit Info</button>
-            <button onClick={() => toggleModal()}>x</button>   
           </div>
           <fieldset className="plant-info">
-            {/* <legend>Plant Info</legend> */}
-            {plantInfo}
+            {labeledAttributes}
+            {labeledSchedules}
           </fieldset>
         </div>
-      </main>
       )
     // else if plant is being edited, render edit plant modal. 
-    } else { 
-      console.log('Edit Plant Form rendered')
-      return (
-        <EditPlantForm 
-          index={index}
-          // get the plant details from the isolated edited plant state object. 
-          plantDetails={editedPlant}
-          editPlant={editPlant}
-          setPlantState={setPlantState}
-          savePlantEdits={savePlantEdits}
-          toggleEditPlant={this.toggleEditPlant}
-        />
-      );
+    // } else { 
+    //   console.log('Edit Plant Form rendered')
+    //   return (
+    //     <EditPlantForm 
+    //       index={index}
+    //       // get the plant details from the isolated edited plant state object. 
+    //       plantDetails={editedPlant}
+    //       editPlant={editPlant}
+    //       setPlantState={setPlantState}
+    //       savePlantEdits={savePlantEdits}
+    //       toggleEditPlant={this.toggleEditPlant}
+    //     />
+    //   );
     }
   }
   
