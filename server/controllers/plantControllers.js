f2330658232bfa0e5336900480b67800ee2f3b03
Gabriel <gkime91@gmail.com>
2023-01-22 19:58:07 -0800

    fixed buggy addPlant and editPlant queries, added rest of data from req bodies.

--- server/controllers/plantControllers.js
+++ server/controllers/plantControllers.js
@@ -32,21 +32,26 @@ const plantControllers = {
   }, 
 
   async addPlant (req, res, next) {
-    const { 
+    const {
       name, 
       img, 
       light, 
       soil, 
       fertilizer, 
-      notes,
-      mist, 
-      fertilizer_schedule,
+      notes, 
+      mist,
       watering_schedule,
-      next_water_date, 
-      next_fertilize_date, 
+      watering_time_of_day,
+      next_water_date,
+      fertilizer_schedule,
+      fertilizing_time_of_day,
+      next_fertilize_date
     } = req.body;
+
     const { days: w_days, weeks: w_weeks, months: w_months } = watering_schedule;
+    const { morning: w_morning, midday: w_midday, evening: w_evening } = watering_time_of_day;
     const { days: f_days, weeks: f_weeks, months: f_months } = fertilizer_schedule;
+    const { morning: f_morning, midday: f_midday, evening: f_evening } = fertilizing_time_of_day;
     try {
       if (!name) throw new Error('name field required');
       const data = await db.query(
@@ -58,18 +63,22 @@ const plantControllers = {
           RETURNING plant_id
         ), w_sched_vals AS (
           INSERT INTO watering_schedule
-          (plant_id, days, weeks, months, next_water_date)
-          SELECT plant_id, $7, $8, $9, $10
+          (plant_id, days, weeks, months, morning, midday, evening, next_water_date)
+          SELECT plant_id, $7, $8, $9, $10, $11, $12, $13
           FROM p_vals
         )
         INSERT INTO fertilizer_schedule
-        (plant_id, days, weeks, months, next_fertilize_date)
-        SELECT plant_id, $11, $12, $13, $14
+        (plant_id, days, weeks, months, morning, midday, evening, next_fertilize_date)
+        SELECT plant_id, $14, $15, $16, $17, $18, $19, $20
         FROM p_vals
-        RETURNING *;`, 
-         [name, light, soil, fertilizer, notes, mist, w_days, w_weeks, w_months, next_water_date, f_days, f_weeks, f_months, next_fertilize_date]); // img
+        RETURNING plant_id;`, 
+        [
+          name, light, soil, fertilizer, notes, mist, w_days, w_weeks, w_months, w_morning,
+          w_midday, w_evening, next_water_date, f_days, f_weeks, f_months, f_morning, f_midday,
+          f_evening, next_fertilize_date
+        ]
+      ); // img
         res.locals.newPlant = data.rows[0];
-        console.log(res.locals.newPlant)
         next();
     } catch(err) {
       console.log(err);
@@ -78,36 +87,45 @@ const plantControllers = {
   },
 
   async editPlant (req, res, next) {
-      const { 
-        plant_id,
-        name, 
-        img, 
-        light, 
-        soil, 
-        fertilizer, 
-        notes,
-        mist, 
-        fertilizer_schedule,
-        watering_schedule,
-        next_water_date, 
-        next_fertilize_date, 
-      } = req.body;
-
+      const {
+      plant_id,
+      name, 
+      img, 
+      light, 
+      soil, 
+      fertilizer, 
+      notes, 
+      mist,
+      watering_schedule,
+      watering_time_of_day,
+      next_water_date,
+      fertilizer_schedule,
+      fertilizing_time_of_day,
+      next_fertilize_date
+    } = req.body;
     const { days: w_days, weeks: w_weeks, months: w_months } = watering_schedule;
-    const { days: f_days, weeks: f_weeks, months: f_months } = fertilizer_schedule;// waterDate, fertilizeDate
+    const { morning: w_morning, midday: w_midday, evening: w_evening } = watering_time_of_day;
+    const { days: f_days, weeks: f_weeks, months: f_months } = fertilizer_schedule;
+    const { morning: f_morning, midday: f_midday, evening: f_evening } = fertilizing_time_of_day;
     try {
       const data = await db.query(
         `WITH p_vals AS (
-          UPDATE plants SET name = $2, img = $3, light = $4, soil = $5, fertilizer = $6, notes = $7, mist = $8
+          UPDATE plants SET name = $2, light = $3, soil = $4, fertilizer = $5, notes = $6, mist = $7
           WHERE plant_id = $1 
           RETURNING plant_id
-        ), f AS (
-          UPDATE watering_schedule SET morning = $9 days = $10, weeks = $11, months = $12, next_water_date = $13 
-          WHERE watering_schedule.plant_id = plant_id;
+        ), w_vals AS (
+          UPDATE watering_schedule SET days = $8, weeks = $9, months = $10, morning = $11, midday = $12, evening = $13, next_water_date = $14 
+          WHERE watering_schedule.plant_id = plant_id
         )
-        UPDATE fertilizer_schedule SET morning = $14 days = $15, weeks = $16, months = $17, next_water_date = $18
-        WHERE fertilizer_schedule.plant_id = plant_id`
-        [plant_id, name, img, light, soil, fertilizer, notes, mist, w_days, w_weeks, w_months, next_water_date, f_days, f_weeks, f_months, next_fertilize_date]);
+        UPDATE fertilizer_schedule SET days = $15, weeks = $16, months = $17, morning = $18, midday = $19, evening = $20,  next_fertilize_date = $21
+        WHERE fertilizer_schedule.plant_id = plant_id
+        RETURNING plant_id;`,
+        [
+          plant_id, name, light, soil, fertilizer, notes, mist, w_days, w_weeks, w_months, w_morning,
+          w_midday, w_evening, next_water_date, f_days, f_weeks, f_months, f_morning, f_midday,
+          f_evening, next_fertilize_date
+        ]
+      ); // img
       res.locals.editedPlant = data.rows[0];
       next();
     } catch(err) {
