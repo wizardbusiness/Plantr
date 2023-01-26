00db401f43fc636dd3bf10557662e87d773272da
Gabriel <gkime91@gmail.com>
2023-01-25 16:16:23 -0800

    refactored methods for building info elements by info type, and made it so that all scheduling info appears on one line.

--- client/components/PlantInfo.jsx
+++ client/components/PlantInfo.jsx
@@ -8,27 +8,28 @@ class PlantInfo extends Component {
     this.state = {
       editPlant: false,
       plantLabelMap: {
-        name: 'Name ',
+        name: 'Name: ',
         img: null,
         light: 'Light: ',
         soil: 'Soil: ',
         fertilizer: 'Fertilizer: ',
         notes: 'Notes: ',
-        watering_schedule: 'Water Every: ',
-        watering_time_of_day: 'Water in the: ',
+        watering_schedule: 'Water Every ',
+        watering_time_of_day: 'in the ',
         days: ' Days',
         weeks: ' Weeks',
         months: ' Months',
-        fertilizer_schedule: 'Fertilize Every: ',
-        fertilizing_time_of_day: 'Fertilize in the: ',
+        fertilizer_schedule: 'Fertilize Every ',
+        fertilize_time_of_day: 'in the ',
         morning: ' Morning',
         midday: ' Afternoon',
         evening: ' Evening'
       },
     }
 
-    this.mapLabelsToProperties = this.mapLabelsToProperties.bind(this);
     this.toggleEditPlant = this.toggleEditPlant.bind(this);
+    this.createInfoFieldElements = this.createInfoFieldElements.bind(this);
+    this.createScheduleElement = this.createScheduleElement.bind(this);
   }
 
   toggleEditPlant() {
@@ -38,7 +39,7 @@ class PlantInfo extends Component {
   };
 
   
-  mapLabelsToProperties () {
+  createInfoFieldElements() {
     const { focusedPlantState } = this.props;
     const labelMap = this.state.plantLabelMap;
     // initial pass: string attributes
@@ -48,7 +49,8 @@ class PlantInfo extends Component {
       const value = focusedPlantState[property];
       if (property in labelMap && typeof value === 'string') primitivePropsWithLabel.push([property, value])
     }
-    const labeledStringAttributes = primitivePropsWithLabel.map((entry, index) => {
+    // map info and info labels to elements
+    const infoFieldElements = primitivePropsWithLabel.map((entry, index) => {
       const key = `att${index}`
       const [ property, value ] = entry;
       const label = labelMap[property];
@@ -61,46 +63,65 @@ class PlantInfo extends Component {
         </label>
       );
     });
-    // second pass: schedule objects
-    // iterate through plant properties and filter for object values.
-    const scheduleObjValues = Object.entries(focusedPlantState).filter((entry) => typeof entry[1] === 'object' && entry[0] !== 'img');
-    const labeledSchedules = scheduleObjValues.map((entry, index) => {
-      const key =`schedule${index}`;
-      const [ stateProperty, scheduleObj] = entry;
-      const label = labelMap[stateProperty];
-      // filter out unused scheduling options
-      const filteredSchedule = Object.entries(scheduleObj).filter(entry => entry[1]);
-        // schedule details
-        const scheduleInfo = filteredSchedule.map((entry, index) => {
-          const [ labelMapProperty, value ] = entry;
-          const key = `scheduleVal${index}`;
-          return (
-            <span key={key}>
-              {value}
-              <label >
-                {labelMap[labelMapProperty]}&nbsp;
-              </label>
-            </span>
-          );
-        });  
-      // label the schedule
-      return(
-        <label key={key}>
-          {label}
-          <span>
-            {scheduleInfo}
-          </span>
-        </label>
-      )
-    });
-
+    console.log(infoFieldElements)
     // return labeled string attributes and labeled schedules
-    return [labeledStringAttributes, labeledSchedules];
+    return infoFieldElements
   };
 
+  createScheduleElement(scheduleType) { 
+    const { focusedPlantState } = this.props;
+    console.log(focusedPlantState)
+    const labelMap = this.state.plantLabelMap; 
+    // iterate through plant properties and filter for object values to create schedule elements.
+    const scheduleObjValues = Object.entries(focusedPlantState).filter((entry) => typeof entry[1] === 'object' && entry[0] !== 'img');
+    const scheduleInfo = scheduleType === 'watering' ? scheduleObjValues.filter(entry => entry[0].match(/watering/)) : 
+      scheduleObjValues.filter(entry => entry[0].match(/fertilize/));
+      console.log(scheduleInfo)
+      // put together all info for schedule on a single line
+    const scheduleElement = scheduleInfo.map((entry, index) => {
+      const key =`schedule${index}`;
+      // each entry has the property name and the schedule object
+      const [ stateProperty, scheduleObj] = entry;
+      // label each schedule using the labelMap
+      const label = labelMap[stateProperty];
+      console.log(label)
+      // filter out unused scheduling options inside the schedule object
+      const filteredSchedule = Object.entries(scheduleObj).filter(entry => entry[1]);
+      // map filtered schedule to jsx component
+      const scheduleProperties = filteredSchedule.map((entry, index) => {
+        // each entry has the property that will be used in the label map to label the value
+        const [ labelMapProperty, value ] = entry;
+        const key = `scheduleVal${index}`;
+        return (
+          <span key={key}>
+            {value}
+            <label>
+              {labelMap[labelMapProperty]}&nbsp;
+            </label>
+          </span>
+        );
+      });  
+      // label the schedule and return the completed component
+      return(
+        <span>
+          <label key={key}>
+            {label}
+          </label>
+          {scheduleProperties}
+        </span>
+      )
+    });
+    return (
+      <span>
+        {scheduleElement}
+      </span>
+    );
+  }
+
   render() {
-    const labeledAttributes = this.mapLabelsToProperties()[0];
-    const labeledSchedules = this.mapLabelsToProperties()[1];
+    const infoFieldElements = this.createInfoFieldElements();
+    const wateringInfo = this.createScheduleElement('watering')
+    const fertilizeInfo = this.createScheduleElement('fertilize')
     const {
       toggleModal,
       editPlant,
@@ -110,6 +131,7 @@ class PlantInfo extends Component {
       resetPlantState,
       setTextfieldState,
       setScheduleState,
+      setTimeOfDayState,
       setMistState,
     } = this.props;
     // if no modal opened (either info modal or edit modal), don't render anything. 
@@ -126,8 +148,9 @@ class PlantInfo extends Component {
             }>Edit</button>
           </div>
           <fieldset className="plant-info">
-            {labeledAttributes}
-            {labeledSchedules}
+            {infoFieldElements}
+            {wateringInfo}
+            {fertilizeInfo}
           </fieldset>
         </div>
       )
@@ -142,6 +165,7 @@ class PlantInfo extends Component {
               resetPlantState={resetPlantState}
               setTextfieldState={setTextfieldState}
               setScheduleState={setScheduleState}
+              setTimeOfDayState={setTimeOfDayState}
               setMistState={setMistState}
               plantState={genericPlantState}
             >
