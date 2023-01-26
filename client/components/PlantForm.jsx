176f6be02f2b58922905a90d0d4a8a90c55c5ea7
Gabriel <gkime91@gmail.com>
2023-01-25 17:55:45 -0800

    reformated schedules to fit in modal.

--- client/components/PlantForm.jsx
+++ client/components/PlantForm.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ScheduleDropdown from './ScheduleDropdown';
+import TimeDropdown from './TimeDropDown';
 import MistCheckbox from './MistCheckbox';
 import PlantFormField from './PlantFormField';
 
@@ -37,7 +38,7 @@ class PlantForm extends Component {
 
   // render whole form, including dropdown and checkbox components. 
   render() {
-    const {submitPlant, btnText, formName, plantState, setScheduleState, setMistState, toggleModal} = this.props;
+    const {submitPlant, btnText, formName, plantState, setScheduleState, setTimeOfDayState, setMistState, toggleModal} = this.props;
     const textFields = this.makeTextFields(); 
     return (
       <form 
@@ -51,23 +52,51 @@ class PlantForm extends Component {
         }>
         <div className='form-text-fields'>
           {textFields}
-        </div>
+        </div> 
         <label>
           Watering Schedule:&nbsp;
-          <ScheduleDropdown
+        </label>
+        <span>
+            <label>
+              Every&nbsp;
+            </label> 
+            <ScheduleDropdown
             setScheduleState={setScheduleState}
             scheduleType='watering_schedule'
             currentSchedule={plantState.watering_schedule}
-          />
-        </label>
+            />
+          
+          <label>
+            in the:&nbsp;
+            <TimeDropdown 
+              setTimeOfDayState={setTimeOfDayState}
+              timeOfDayState={plantState.watering_time_of_day}
+              stateObjName='watering_time_of_day'
+            />
+          </label>
+        </span>
+        
         <label>
           Fertilizer Schedule:&nbsp;
+        </label>
+        <span>
+          <label>
+            Every&nbsp;
+          </label>
           <ScheduleDropdown 
             setScheduleState={setScheduleState}
             scheduleType='fertilizer_schedule'
             currentSchedule={plantState.fertilizer_schedule}
           />
+        <label>
+          in the:&nbsp;
+          <TimeDropdown 
+            setTimeOfDayState={setTimeOfDayState}
+            timeOfDayState={plantState.fertilize_time_of_day}
+            stateObjName='fertilize_time_of_day'
+          />
         </label>
+        </span>  
         <label>
           Mist:&nbsp;
           <MistCheckbox value={plantState.mist} setMistState={setMistState}/>
