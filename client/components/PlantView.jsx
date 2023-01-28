88c3739d013ea5cb9f4c331c4c6be9070072baa3
Gabriel <gkime91@gmail.com>
2023-01-28 00:51:04 -0800

    substantially changed schedule state and methods to allow for more intuitive options and logic when creating a schedule. Specifically added a default unselected schedule option for each value dropdown in the plant creation and editing form. also added state to store plant species and made it required, while removing the requirement for the name to be entered. This should be more useful for the user.

--- client/components/PlantView.jsx
+++ client/components/PlantView.jsx
@@ -6,15 +6,11 @@ class PlantView extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      editPlant: {
-        saveEdit: true,
-      },
       plants: [],
-      editedPlant: {},
       // plant properties relate to columns in plants table in postgreSQL db. 
-      // columns in db: plant_id, name, img, light, soil, fertilizer, notes, day, week, month, morning, evening, midday, mist, next_water_date, next_fertilize_date
       // note that plant_id is defined in database, not new plant state. 
       plant: {
+        plant_species: '',
         name: '',
         img: '',
         light: '',
@@ -22,30 +18,34 @@ class PlantView extends Component {
         fertilizer: '',
         notes: '',
         watering_schedule: {
+          unselected_date: true,
           days: 0,
           weeks: 0, 
           months: 0
         },
         fertilizer_schedule: {
+          unselected_date: true, 
           days: 0,
           weeks: 0,
           months: 0
         },
         watering_time_of_day: {
-          default: true,
+          unselected_tod: true,
           morning: false,
           midday: false,
           evening: false
         },
         fertilize_time_of_day: {
-          default: true,
+          unselected_tod: true,
           morning: false,
           midday: false,
           evening: false
         },
         mist: false,
-        next_water_date: new Date(),
-        next_fertilize_date: new Date()
+        initial_water_date: null,
+        next_water_date: null,
+        initial_fertilize_date: null,
+        next_fertilize_date: null,
       },
     }
     this.processDbPlantForFrontendState = this.processDbPlantForFrontendState.bind(this);
@@ -56,8 +56,7 @@ class PlantView extends Component {
     this.setTextfieldState = this.setTextfieldState.bind(this);
     this.setMistState = this.setMistState.bind(this);
     this.resetPlantState = this.resetPlantState.bind(this);
-    this.createDateFromSchedule = this.createDateFromSchedule.bind(this);
-    this.checkSchedule = this.checkSchedule.bind(this);
+    this.createDatesFromSchedule = this.createDatesFromSchedule.bind(this);
     this.addPlant = this.addPlant.bind(this);
     this.editPlant = this.editPlant.bind(this);
     this.clonePlant = this.clonePlant.bind(this);
@@ -81,13 +80,16 @@ class PlantView extends Component {
   };
 
 
-  createDateFromSchedule(scheduleObj, dateDesc) {
+  createDatesFromSchedule(scheduleObj, typeOfScheduledDate, typeOfInitialDate) {
+    if (scheduleObj.unselected_tod === true) return null;
     const scheduledTime = Object.entries(this.state.plant.watering_time_of_day).filter(entry => entry[1])[0][0];
     // schedule interval in days
     let intervalInDays = 0;
     // get current date
     // console.log(scheduleObj)
     const scheduledDate = new Date();
+    // only change the initial date if there is none in the plant state. 
+    const initialDate = this.state.plant[typeOfInitialDate] ? this.state.plant[typeOfInitialDate] : new Date();
     // iterate through schedule dropDown
     for (let interval in scheduleObj) {
     // convert all values to days and add to intervalInDays
@@ -108,25 +110,11 @@ class PlantView extends Component {
       ...this.state,
       plant: {
         ...this.state.plant,
-        [dateDesc]: scheduledDate
+        [typeOfScheduledDate]: scheduledDate,
+        [typeOfInitialDate]: initialDate !== this.state.plant[initialDate] ? initialDate : this.state.plant[initialDate]
       }
     });
-
     return scheduledDate;
-  }
-
-  checkSchedule(watering_schedule) { 
-    // create new current watering_schedule. 
-    // convert both dates to milliseconds
-    const check = setInterval(() => {
-      const currentDate = new Date().getTime();
-      const scheduledDate = new Date(watering_schedule).getTime();
-      // if scheduled watering_schedule is less than current watering_schedule, {css logic} (for now just console log that plant needs watering or fertilizing)
-      const result = scheduledDate <= currentDate ? console.log('plant needs watering!') : false;
-      return result;
-    }, 3000)
-
-    return check;
   }
 
   processDbPlantForFrontendState(plants) {
@@ -145,14 +133,14 @@ class PlantView extends Component {
           const processedProperty = property.slice(2);
           if (processedProperty === 'days' || processedProperty === 'weeks' || processedProperty === 'months' ) {
             processedPlantObj.watering_schedule[processedProperty] = plant[property];
-          } else if (processedProperty === 'default' || processedProperty === 'morning' || processedProperty === 'midday' || processedProperty === 'evening') {
+          } else if (processedProperty === 'unselected_tod' || processedProperty === 'morning' || processedProperty === 'midday' || processedProperty === 'evening') {
             processedPlantObj.watering_time_of_day[processedProperty] = plant[property];
           };
         } else if (property[0] === 'f') {
           const processedProperty = property.slice(2);
           if (processedProperty === 'days' || processedProperty === 'weeks' || processedProperty === 'months' ) {
             processedPlantObj.fertilizer_schedule[processedProperty] = plant[property];
-          } else if (processedProperty === 'default' || processedProperty === 'morning' || processedProperty === 'midday' || processedProperty === 'evening') {
+          } else if (processedProperty === 'unselected_tod' || processedProperty === 'morning' || processedProperty === 'midday' || processedProperty === 'evening') {
             processedPlantObj.fertilize_time_of_day[processedProperty] = plant[property];
           };
         };
@@ -167,7 +155,7 @@ class PlantView extends Component {
     try {
       const response = await fetch('/plants');
       const plants = await response.json();
-      console.log(this.processDbPlantForFrontendState(plants))
+      console.log(this.processDbPlantForFrontendState(plants));
       // convert plant state in db back to state structure in react state. 
      this.setState({
         ...this.state.plants,
@@ -185,6 +173,7 @@ class PlantView extends Component {
       plant: {
         ...this.state.plant, 
         // reset new plant state object to default values.
+        plant_species: '',
         name: '',
         img: '',
         light: '',
@@ -205,56 +194,83 @@ class PlantView extends Component {
         },
         watering_time_of_day: {
           ...this.state.plant.watering_time_of_day,
-          default: true, 
+          unselected_tod: true, 
           morning: false,
           midday: false,
           evening: false
         },
         fertilize_time_of_day: {
           ...this.state.plant.fertilize_time_of_day,
-          default: true,
+          unselected_tod: true,
           morning: false,
           midday: false,
           evening: false
         },
         mist: false,
-        next_water_date: new Date(),
-        next_fertilize_date: new Date(),
+        initial_water_date: null,
+        next_water_date: null,
+        next_fertilize_date: null,
+        initial_fertilize_date: null
       },
     });
   }
   // SET PLANT STATE: Updates properties of a new or edited plant in state when the user fills out the new or edited plant form.
   // args: property being updated, updated value.  
-  setScheduleState(scheduleType, dateUnit, value) {
-    if (scheduleType === 'watering_schedule') {
-      this.setState({
-        ...this.state,
-        plant: {
-          ...this.state.plant,
-          [scheduleType]: {
-            ...this.state.plant[scheduleType],
-            [dateUnit]: value,
+  async setScheduleState(scheduleType, dateUnit, value) {
+    // if (scheduleType === 'watering_schedule') {
+      if (value.includes('set')) {
+        this.setState({
+          ...this.state,
+          plant: {
+            ...this.state.plant,
+            [scheduleType]: {
+              ...this.state.plant[scheduleType],
+              unselected_date: true,
+              [dateUnit]: 0 
+            }
           }
-        }
-      });
-    } else if (scheduleType === 'fertilizer_schedule') {
-      this.setState({
-        ...this.state,
-        plant: {
-          ...this.state.plant,
-          [scheduleType]: {
-            ...this.state.plant[scheduleType],
-            [dateUnit]: value,
+        });
+      } else {
+        // do a check if time of day has been set, if it hasn't set it to morning automatically when any schedule is set.
+        const checkWaterTod = Object.entries(this.state.plant.watering_time_of_day).filter(entry => entry[1])[0][0];
+        const checkFertilizeTod =  Object.entries(this.state.plant.fertilize_time_of_day).filter(entry => entry[1])[0][0];
+        if (scheduleType.includes('water') && checkWaterTod === 'unselected_tod') {
+          await this.setTimeOfDayState('morning', 'watering_time_of_day');
+        } else if (scheduleType.includes('fert') && checkFertilizeTod === 'unselected_tod') {
+          await this.setTimeOfDayState('morning', 'fertilize_time_of_day');
+        } 
+        // then proceed with setting the schedule state
+        this.setState({
+          ...this.state,
+          plant: {
+            ...this.state.plant,
+            [scheduleType]: {
+              ...this.state.plant[scheduleType],
+              unselected_date: false,
+              [dateUnit]: value
+            }
           }
-        }
-      });
-    };
+        }, () => console.log(this.state.plant.watering_time_of_day));
+      }
+    // }
+    //  else if (scheduleType === 'fertilizer_schedule') {
+    //   this.setState({
+    //     ...this.state,
+    //     plant: {
+    //       ...this.state.plant,
+    //       [scheduleType]: {
+    //         ...this.state.plant[scheduleType],
+    //         [dateUnit]: value,
+    //       }
+    //     }
+    //   });
+    // };
     return; 
   }
 
   setTimeOfDayState(chosenValue, stateObjName) {
     const newTimeOfDayState = {
-      default: false,
+      unselected_tod: false,
       morning: false,
       midday: false,
       evening: false
@@ -264,15 +280,14 @@ class PlantView extends Component {
     //   if (property === chosenValue) Object.assign{{},  ...newTimeOfDayState, property }
     //   // else newTimeOfDayState = timeOfDayState[property]
     // }
-    const modifiedState = Object.assign(newTimeOfDayState, chosenValueObj)
+    const modifiedState = {...newTimeOfDayState, ...chosenValueObj}
     this.setState({
       ...this.state,
       plant: {
         ...this.state.plant,
         [stateObjName]: modifiedState
       }
-    }, () => console.log(this.state.plant[stateObjName]));
-        
+    });
   }
 
   setTextfieldState(name, value) {
@@ -300,6 +315,7 @@ class PlantView extends Component {
     e.preventDefault();
     const { 
       name, 
+      plant_species,
       img, 
       light, 
       soil, 
@@ -308,16 +324,19 @@ class PlantView extends Component {
       mist,
       watering_time_of_day,
       watering_schedule, 
+      initial_water_date,
       fertilize_time_of_day,
       fertilizer_schedule,
+      initial_fertilize_date,
     } = this.state.plant;
     // dates
-    const next_water_date = await this.createDateFromSchedule(watering_schedule, 'next_water_date');
-    const next_fertilize_date = await(this.createDateFromSchedule(fertilizer_schedule, 'next_fertilize_date'))
+    const next_water_date = await this.createDatesFromSchedule(watering_schedule, 'next_water_date', 'initial_water_date');
+    const next_fertilize_date = await(this.createDatesFromSchedule(fertilizer_schedule, 'next_fertilize_date', 'initial_fertilize_date'))
 
     // add all values from destructured state to request body
     const body = {
       name, 
+      plant_species,
       img, 
       light, 
       soil, 
@@ -326,9 +345,11 @@ class PlantView extends Component {
       mist,
       watering_schedule,
       watering_time_of_day,
+      initial_water_date,
       next_water_date,
       fertilizer_schedule,
       fertilize_time_of_day,
+      initial_fertilize_date,
       next_fertilize_date
     };
     try {
@@ -362,7 +383,7 @@ class PlantView extends Component {
     this.setState({
       ...this.state,
       plant: plant
-    })      
+    });      
   }
 
   // CLONE PLANT: clones the plant being edited to location in state. 
@@ -380,6 +401,8 @@ class PlantView extends Component {
     e.preventDefault();
     // destructure state
     const { 
+      plant_id,
+      plant_species,
       name, 
       img, 
       light, 
@@ -392,12 +415,14 @@ class PlantView extends Component {
       fertilize_time_of_day,
       fertilizer_schedule,
     } = this.state.plant;
-    const { plant_id } = this.state.plant;
-    const next_water_date = await this.createDateFromSchedule(watering_schedule, 'next_water_date');
-    const next_fertilize_date = await(this.createDateFromSchedule(fertilizer_schedule, 'next_fertilize_date'))
+    // calculate changes to schedule from the initial date the schedule was last set. so if the schedule was set to water every 2 days,
+    // and it's changed to three, it should add 3 days from the initial date the schedule was set. 
+    const next_water_date = await this.createDatesFromSchedule(watering_schedule, 'next_water_date', 'initial_water_date');
+    const next_fertilize_date = await(this.createDatesFromSchedule(fertilizer_schedule, 'next_fertilize_date', 'initial_fertilize_date'))
     // add all values from destructured state to request body
     const body = {
       plant_id,
+      plant_species,
       name, 
       img, 
       light, 
@@ -419,8 +444,7 @@ class PlantView extends Component {
           'Content-Type': 'Application/JSON'
         },
         body: JSON.stringify(body)
-      })
-
+      });
       // wait for the database to send back a response before proceeding.
       const dbResponseOk = await response.json();
       const plants = this.state.plants;
@@ -476,7 +500,6 @@ class PlantView extends Component {
           setMistState={this.setMistState}
           setTimeOfDayState={this.setTimeOfDayState}
           resetPlantState={this.resetPlantState}
-          checkSchedule={this.checkSchedule}
           editPlant={this.editPlant}
           savePlantEdits={this.savePlantEdits}
           deletePlant={this.deletePlant}
--- server/controllers/plantControllers.js
+++ server/controllers/plantControllers.js
@@ -5,6 +5,7 @@ const plantControllers = {
     try { 
       const data = await db.query(
         `SELECT pl.*, 
+          f.unselected_tod as f_unselected_tod,
           f.days AS f_days, 
           f.weeks AS f_weeks, 
           f.months AS f_months, 
@@ -12,13 +13,16 @@ const plantControllers = {
           f.midday AS f_midday,
           f.evening AS f_evening,
           next_fertilize_date,
+          initial_fertilize_date,
+          w.unselected_tod AS w_unselected_tod,
           w.days AS w_days, 
           w.weeks AS w_weeks, 
           w.months AS w_months,
           w.morning AS w_morning,
           w.midday AS w_midday,
           w.evening AS w_evening,
-          next_water_date
+          next_water_date,
+          initial_water_date
         FROM plants pl 
         JOIN fertilizer_schedule f ON pl.plant_id = f.plant_id
         JOIN watering_schedule w on pl.plant_id = w.plant_id;
@@ -33,6 +37,7 @@ const plantControllers = {
 
   async addPlant (req, res, next) {
     const {
+      plant_species,
       name, 
       img, 
       light, 
@@ -43,39 +48,41 @@ const plantControllers = {
       watering_schedule,
       watering_time_of_day,
       next_water_date,
+      initial_water_date,
       fertilizer_schedule,
-      fertilizing_time_of_day,
-      next_fertilize_date
+      fertilize_time_of_day,
+      next_fertilize_date,
+      initial_fertilize_date,
     } = req.body;
-
     const { days: w_days, weeks: w_weeks, months: w_months } = watering_schedule;
-    const { morning: w_morning, midday: w_midday, evening: w_evening } = watering_time_of_day;
+    const { unselected_tod: w_unselected_tod, morning: w_morning, midday: w_midday, evening: w_evening } = watering_time_of_day;
     const { days: f_days, weeks: f_weeks, months: f_months } = fertilizer_schedule;
-    const { morning: f_morning, midday: f_midday, evening: f_evening } = fertilizing_time_of_day;
+    const { unselected_tod: f_unselected_tod, morning: f_morning, midday: f_midday, evening: f_evening } = fertilize_time_of_day;
+    console.log(f_unselected_tod)
     try {
-      if (!name) throw new Error('name field required');
+      if (!plant_species) throw new Error('plant_species field required');
       const data = await db.query(
         `WITH p_vals AS (
           INSERT INTO plants
-          (name, light, soil, fertilizer, notes, mist)
+          (plant_species, name, light, soil, fertilizer, notes, mist)
           VALUES
-          ($1, $2, $3, $4, $5, $6)
+          ($1, $2, $3, $4, $5, $6, $7)
           RETURNING plant_id
         ), w_sched_vals AS (
           INSERT INTO watering_schedule
-          (plant_id, days, weeks, months, morning, midday, evening, next_water_date)
-          SELECT plant_id, $7, $8, $9, $10, $11, $12, $13
+          (plant_id, days, weeks, months, unselected_tod, morning, midday, evening, next_water_date, initial_water_date)
+          SELECT plant_id, $8, $9, $10, $11, $12, $13, $14, $15, $16
           FROM p_vals
         )
         INSERT INTO fertilizer_schedule
-        (plant_id, days, weeks, months, morning, midday, evening, next_fertilize_date)
-        SELECT plant_id, $14, $15, $16, $17, $18, $19, $20
+        (plant_id, days, weeks, months, unselected_tod, morning, midday, evening, next_fertilize_date, initial_fertilize_date)
+        SELECT plant_id, $17, $18, $19, $20, $21, $22, $23, $24, $25
         FROM p_vals
         RETURNING plant_id;`, 
         [
-          name, light, soil, fertilizer, notes, mist, w_days, w_weeks, w_months, w_morning,
-          w_midday, w_evening, next_water_date, f_days, f_weeks, f_months, f_morning, f_midday,
-          f_evening, next_fertilize_date
+          plant_species, name, light, soil, fertilizer, notes, mist, w_days, w_weeks, w_months, w_unselected_tod, w_morning,
+          w_midday, w_evening, next_water_date, initial_water_date, f_days, f_weeks, f_months, f_unselected_tod, f_morning, f_midday, f_evening, 
+          next_fertilize_date, initial_fertilize_date
         ]
       ); // img
         res.locals.newPlant = data.rows[0];
@@ -89,6 +96,7 @@ const plantControllers = {
   async editPlant (req, res, next) {
       const {
       plant_id,
+      plant_species,
       name, 
       img, 
       light, 
@@ -99,31 +107,34 @@ const plantControllers = {
       watering_schedule,
       watering_time_of_day,
       next_water_date,
+      initial_water_date,
       fertilizer_schedule,
-      fertilizing_time_of_day,
-      next_fertilize_date
+      fertilize_time_of_day,
+      next_fertilize_date,
+      initial_fertilize_date
     } = req.body;
-    const { days: w_days, weeks: w_weeks, months: w_months } = watering_schedule;
+    const { unselected: w_unselected_tod, days: w_days, weeks: w_weeks, months: w_months } = watering_schedule;
     const { morning: w_morning, midday: w_midday, evening: w_evening } = watering_time_of_day;
     const { days: f_days, weeks: f_weeks, months: f_months } = fertilizer_schedule;
-    const { morning: f_morning, midday: f_midday, evening: f_evening } = fertilizing_time_of_day;
+    const { unselected: f_unselected_tod, morning: f_morning, midday: f_midday, evening: f_evening } = fertilize_time_of_day;
     try {
+      if (!plant_species) throw new Error('plant_species field required')
       const data = await db.query(
         `WITH p_vals AS (
-          UPDATE plants SET name = $2, light = $3, soil = $4, fertilizer = $5, notes = $6, mist = $7
+          UPDATE plants SET plant_species = $2, name = $3, light = $4, soil = $5, fertilizer = $6, notes = $7, mist = $8
           WHERE plant_id = $1 
           RETURNING plant_id
         ), w_vals AS (
-          UPDATE watering_schedule SET days = $8, weeks = $9, months = $10, morning = $11, midday = $12, evening = $13, next_water_date = $14 
+          UPDATE watering_schedule SET days = $9, weeks = $10, months = $11, unselected_tod = $12, morning = $13, midday = $14, evening = $15, next_water_date = $16, initial_water_date = $17 
           WHERE watering_schedule.plant_id = plant_id
         )
-        UPDATE fertilizer_schedule SET days = $15, weeks = $16, months = $17, morning = $18, midday = $19, evening = $20,  next_fertilize_date = $21
+        UPDATE fertilizer_schedule SET days = $18, weeks = $19, months = $20, unselected_tod = $21, morning = $22, midday = $23, evening = $24,  next_fertilize_date = $25, initial_fertilize_date = $26
         WHERE fertilizer_schedule.plant_id = plant_id
         RETURNING plant_id;`,
         [
-          plant_id, name, light, soil, fertilizer, notes, mist, w_days, w_weeks, w_months, w_morning,
-          w_midday, w_evening, next_water_date, f_days, f_weeks, f_months, f_morning, f_midday,
-          f_evening, next_fertilize_date
+          plant_id, plant_species, name, light, soil, fertilizer, notes, mist, w_days, w_weeks, w_months, w_unselected_tod, w_morning,
+          w_midday, w_evening, next_water_date, initial_water_date, f_unselected_tod, f_days, f_weeks, f_months, f_morning, f_midday,
+          f_evening, next_fertilize_date, initial_fertilize_date
         ]
       ); // img
       res.locals.editedPlant = data.rows[0];
