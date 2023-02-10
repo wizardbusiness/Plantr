d6c9bfa0ae647e70c184ea18b5f0b1bc9a58b3ad
Gabriel <gkime91@gmail.com>
2023-02-09 22:52:18 -0800

    added watering can component for watering plants.

---
+++ client/components/WateringCan.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../src/styles.css';
+
+
+export default function WateringCan({waterPlant, handleWaterPlant}) {
+  // when watering can is clicked, it will set watering mode to true. 
+  // when watering mode is true, clicking on a plant will fire the waterPlant method. 
+  // this will set the initial date watered to current, and rebuild the schedule based off what has been set by the user.
+  return (
+    // style={{"--active": waterPlant ? "#88d1fb;" : "#2ac9c4"}}
+    <div className="watering-can" onClick={() => handleWaterPlant()} style={{"--active": waterPlant ? "#0047b8" : "#6cf8d1"}} ></div>
+  )
+}
