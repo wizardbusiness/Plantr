888c15fdd008fab6027275793b60545884615c43
Gabriel <gkime91@gmail.com>
2023-04-04 11:16:37 -0700

    added key to shelf components.

--- client/components/Shelf.jsx
+++ client/components/Shelf.jsx
@@ -4,9 +4,10 @@ function Shelf({plants}) {
 
   function buildShelf() {
     // builds a shelf section for each plant
-    return plants.map(plant => {
+    return plants.map((plant, index) => {
+      const key = `s${index}`
       return (
-      <div className='shelf'>
+      <div className='shelf' key={key}>
         <img height='25px' width='205px' src='images/shelfSection.svg'/>
       </div>
       )
